5d5e94c99c58abb27faad1b5874591cbe208bf91
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-12-19 13:53:33 +0300

    feat: add conditional rendering to productcard

--- product_showcase/src/components/productcard.jsx
+++ product_showcase/src/components/productcard.jsx
@@ -3,9 +3,30 @@ export function Productcard({products}){
     return (
         products.map(product=>(
             <div key={product.id}>
-                <img src={product.image} width="250px"/>
+
+                {/* condition to check if there is an Image for any product before rendering */}
+                {product.image != "" && (
+                    <img src={product.image} width="250px"/>
+                )}
+                
                 <p>  Product: {product.name}</p>
                 <p>  Price: {product.price}💰</p>
+
+                {/* condition to check if the product is premium or not */}
+                {product.price > 100000 && (
+                    <p>✨ Premium Product</p>
+                )}
+
+                {/* condition to check if the product is in stock */}
+                {product.instock ? (
+                    <p>Instock</p>
+                ):
+                <p>Out of stock</p>}
+
+                {/* condition to check if the product is featured or not */}
+                {product.isfeatured && (
+                    <p>Featured</p>
+                )}
             </div> 
         )))
 
