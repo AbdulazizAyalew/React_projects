9f713eaa63a54ab39b92a886c2a0c33e07b9ada7
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-12-19 14:29:19 +0300

    feat: handle empty product list state

--- product_showcase/src/components/Product_list.jsx
+++ product_showcase/src/components/Product_list.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Productcard } from "./productcard";
 
 function ProductList({products}){
+    if(products.length < 1){
+        return <p>No Products available at the Moment</p>;
+    }
     return (
         products.map(product=>(
             <Productcard product = {product}/>
