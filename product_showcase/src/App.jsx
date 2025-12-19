790d6c6d8ae5e02120885a6822fae33173827d40
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-12-19 12:02:44 +0300

    feat: add static product data source

--- product_showcase/src/App.jsx
+++ product_showcase/src/App.jsx
@@ -1,34 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createRoot } from "react-dom/client"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const products = [
+    {id:1,name:"Asus",price:"100,000",Image:"",instock:true,isfeatured:false},
+    {id:2,name:"MSI",price:"120,000",Image:"",instock:false,isfeatured:false},
+    {id:3,name:"ENVY",price:"150,000",Image:"",instock:true,isfeatured:true},
+    {id:4,name:"Elitebook",price:"50,000",Image:"",instock:true,isfeatured:true},
+  ]
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const root = createRoot(document.getElementById("root"))
+  root.render(
+    
   )
 }
 
