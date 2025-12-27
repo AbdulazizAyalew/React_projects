20f1cfa1de2acb341dcdc7ec22f4787338d8bf8e
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-12-27 22:57:35 +0300

    feat: Add Home page with welcome text and movie list

--- movie_database/movie_database/src/components/Home.jsx
+++ movie_database/movie_database/src/components/Home.jsx
@@ -1,6 +1,45 @@
 import React from "react";
+import "../App.css"
 
+
+var movies = [
+  {
+    name: "Stranger Things",
+    Genre: "Sci-fi,Horror",
+    Seasons: "5",
+    Rating: "10/10",
+    image: "/images/Stranger_things.jpg",
+  },
+  {
+    name: "Power",
+    Genre: "Drama",
+    Seasons: "6",
+    Rating: "9.8/10",
+    image: "/images/Power.jpg",
+  },
+  {
+    name: "Game of Thrones",
+    Genre: "Drama, Thriller",
+    Seasons: "8",
+    Rating: "9.9/10",
+    image: "/images/Got.jpg",
+  },
+];
 function Home(){
-    return 
+    return (
+      <>
+        <p>Welcome To My Movie List</p>
+        <div>
+          {movies.map((movie) => (
+            <div className="Movies">
+              <img src={movie.image} alt={movie.name} width="200" />
+              <p>{movie.name}</p>
+            </div>
+          ))}
+        </div>
+      </>
+    ); 
+        
+        
 }
 export default Home
