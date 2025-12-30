451fd376870238df86f4b6157bef3b0bc51117af
AbdulazizAya <abdulazizayalewmussa@gmail.com>
2025-12-30 09:57:23 +0300

    feat: Add genre Based movie count using Reduce

--- movie_database/src/components/Search.jsx
+++ movie_database/src/components/Search.jsx
@@ -3,21 +3,33 @@ import { useSearchParams } from "react-router-dom";
 
 function Search({movies}){
     const [searchparam] = useSearchParams();
-    const Genre = searchparam.get("Genre")
+    const Genre = searchparam.get("Genre").toLowerCase();
 
-    return (
-      <div>
-        <p>Current Genre: {Genre}</p>
-        {movies.map(
-          (movie) =>
-            movie.Genre.toLowerCase() == Genre.toLowerCase() && (
-              <>
-                <img src={movie.image} alt={movie.name} width="200" />
-                <p>{movie.name}</p>
-              </>
-            )
-        )}
-      </div>
-    );
+    const count_filtered = movies.reduce(
+  (count, movie) => movie.Genre.toLowerCase() === Genre ? count + 1 : count,
+  0
+);
+
+
+    if(count_filtered == 0){
+      return <p>No Movie Found!!</p>
+    }
+    else{
+      return (
+        <div>
+          <p>Current Genre: {Genre}</p>
+          {movies.map(
+            (movie) =>
+              movie.Genre.toLowerCase() == Genre && (
+                <>
+                  <img src={movie.image} alt={movie.name} width="200" />
+                  <p>{movie.name}</p>
+                </>
+              )
+          )}
+        </div>
+      );
+    }
+    
 }
 export default Search
